65698f3eb35804445315aa5890be8a6274eb1cf8
bionic-chipset-2986 <janaeelisa@gmail.com>
2019-08-05 23:30:19 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,6 +3,8 @@ def position_taken?(board, index)
   position = board [index]
   if position == nil
     false
+  elsif position.upcase != "X" and position.upcase != "O"
+    false
   else
     true
   end
