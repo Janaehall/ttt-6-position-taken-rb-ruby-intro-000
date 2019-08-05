beae94914ad1796da73b562dfba5a024c99c442b
bionic-chipset-2986 <janaeelisa@gmail.com>
2019-08-05 23:33:40 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
   position = board [index]
-  if position == nil
+  if position.nil
+    puts "hiiiiiiiiii"
     false
   elsif position.upcase != "X" and position.upcase != "O"
     false
